a7adc70f53f5b001ba5aaf67eb4d213f57c10239
Isaiah Murray <murray.isaiah13@gmail.com>
2020-03-25 14:29:51 -0400

    Added NoteEdit

--- ElevenNote.WebMVC/App_Start/RouteConfig.cs
+++ ElevenNote.WebMVC/App_Start/RouteConfig.cs
@@ -14,9 +14,13 @@ namespace ElevenNote.WebMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Default", //The default parameter is currently set to open the application on the Home page.
                 url: "{controller}/{action}/{id}",
+                //{action} is the ActionResult we are calling on (Create, Details, Edit, or Delete).
+                //{id} is an optional parameter that will only be used when we are working with a specific note. 
+                //We'll add the NoteId to the end of the Url in these cases.
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //url: is what builds out the Url we see on the webpage. 
             );
         }
     }
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -52,5 +52,27 @@ namespace ElevenNote.WebMVC.Controllers
 
             return View(model);
         }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+            return View(model);
+        }
     }
 }
